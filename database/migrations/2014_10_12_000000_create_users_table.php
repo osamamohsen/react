c1df53fcbf169decc41ab5734f6b474bc5dcae09
Osama Mohsen <o.mohsen@shiftebusiness.com>
2017-03-06 12:01:29 +0200

    add new

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('image')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
